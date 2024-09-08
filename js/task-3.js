e26f2522274ca3d3d70acc6501dcf818e3dcc12e
Oleksii Piliai <alekseivpiliai@gmail.com>
2024-09-08 20:22:08 +0200

    start1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,15 @@
-function getElementWidth(content, padding, border) {
-  const numericContent = parseFloat(content);
-  const numericPadding = parseFloat(padding);
-  const numericBorder = parseFloat(border);
-
-  const totalWidth = numericContent + ((numericPadding + numericBorder) * 2);
-  return totalWidth;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+  return filteredNumbers;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
